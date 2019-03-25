1c560e6e4e458ea0748c1d5a05201b7cd3c9926c
Loic CHETY <lchety@e1r5p10.42.fr>
2019-03-25 10:58:40 +0100

    wip

--- client/src/spawnerScript.js
+++ client/src/spawnerScript.js
@@ -16,8 +16,8 @@ class SpawnerScript {
 		this.letterQuantity = 0;
 		this.alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 		this.nextChangeSpeed = Date.now() + 10000;
-		this.changeSpeedDelay = 8000;
-		this.speedReduce = 8;
+		this.changeSpeedDelay = 4000;
+		this.speedReduce = 20;
 	}
 
 	update() {
